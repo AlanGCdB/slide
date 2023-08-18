3a7c6f8f7376edab02f7448eaa5961a5c68487cc
AlanGCdB <dev.alancarlos@gmail.com>
2023-08-18 13:18:45 -0300

    Calculo de distancias quando os eventos ocorren

--- js/slide.js
+++ js/slide.js
@@ -2,36 +2,48 @@ export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
-        
+        this.dist = { finalPosition: 0, stratX: 0, movement: 0 }
     }
-onStrat(event){
-event.preventDefault();
-this.wrapper.addEventListener('mousemove', this.onMove);
-}
 
-onMove(event){
-}
-onEnd(event){
-this.wrapper.removeEventListener('mousemove', this.onMove);
+    moveSlide(distX) {
+        this.dist.movePosition = distX;
+        this.slide.style.transform = `translate3d(${distX}px,0,0)`;
+    }
 
+    updatePosition(clientX) {
+        this.dist.movement = (this.dist.stratX - clientX) * 3;
+        return this.dist.finalPosition - this.dist.movement;
+    }
 
-}
+    onStrat(event) {
+        event.preventDefault();
+        this.dist.stratX = event.clientX;
+        this.wrapper.addEventListener('mousemove', this.onMove);
+    }
 
-addSlideEvents(){
-    this.wrapper.addEventListener('mousedown', this.onStrat);
-    this.wrapper.addEventListener('mouseup', this.onEnd);
-}
-bindEvents(){
-    this.onStrat = this.onStrat.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-}
+    onMove(event) {
+        const finalPosition = this.updatePosition(event.clientX);
+        this.moveSlide(finalPosition);
+    }
 
-init(){
-    this.bindEvents();
-    this.onMove();
-    this.onEnd();
-    this.addSlideEvents();
-    return this;
-}
+    onEnd(event) {
+        this.wrapper.removeEventListener('mousemove', this.onMove);
+        this.dist.finalPosition = this.dist.movePosition;
+    }
+
+    addSlideEvents() {
+        this.wrapper.addEventListener('mousedown', this.onStrat);
+        this.wrapper.addEventListener('mouseup', this.onEnd);
+    }
+    bindEvents() {
+        this.onStrat = this.onStrat.bind(this);
+        this.onMove = this.onMove.bind(this);
+        this.onEnd = this.onEnd.bind(this);
+    }
+
+    init() {
+        this.bindEvents();
+        this.addSlideEvents();
+        return this;
+    }
 }
